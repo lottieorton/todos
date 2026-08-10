4cc52aff9ecf9da566e1f6d22edae06eeb184501
LottieOrton <lottieorton11@gmail.com>
2026-08-10 12:31:54 +1000

    initial setup with database connection

---
+++ src/main/java/io/nology/todos/category/entities/Category.java
@@ -0,0 +1,35 @@
+package io.nology.todos.category.entities;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name = "categories")
+public class Category {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column
+    private String type;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getType() {
+        return type;
+    }
+
+    public void setType(String type) {
+        this.type = type;
+    }
+}
